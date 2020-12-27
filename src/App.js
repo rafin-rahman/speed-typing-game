176e114b88c40e4598f4f43eec773a65ba0e79c6
Rafin Rahman <ra-fin@live.com>
2020-12-27 02:56:41 +0000

    UI completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./style.css";
 
 function App() {
+  const [text, setText] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(5);
+  const [startGame, setStartGame] = useState(false);
+
+  function handleChange(event) {
+    const { value } = event.target;
+    setText(value);
+  }
+  function handleClick() {
+    console.log("working");
+    setStartGame((prevState) => (prevState = true));
+  }
+  useEffect(() => {
+    if (startGame) {
+      setTimeout(() => {
+        if (timeRemaining !== 0) {
+          setTimeRemaining((time) => time - 1);
+        } else {
+          return timeRemaining;
+        }
+      }, 1000);
+    }
+  }, [timeRemaining]);
+
+  function calcWordNumber(words) {
+    const number = words.trim().split(" ");
+    const filteredWords = number.filter((word) => word !== "");
+    console.log(filteredWords.length);
+    return filteredWords.length;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>HOW FAST CAN YOU TYPE?</h1>
+
+      <textarea name={"text"} value={text} onChange={handleChange} />
+      <h4>Time left: {timeRemaining}</h4>
+
+      <button onClick={handleClick}>START</button>
+      <h1>WORD COUNT: </h1>
     </div>
   );
 }
