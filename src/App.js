02fb8d7151ae230cf66e456fb2e6b17458c46668
Rafin Rahman <ra-fin@live.com>
2020-12-27 03:59:54 +0000

    Minimum viable product ready

--- src/App.js
+++ src/App.js
@@ -2,30 +2,31 @@ import React, { useState, useEffect } from "react";
 import "./style.css";
 
 function App() {
+  const timerVar = 5;
   const [text, setText] = useState("");
-  const [timeRemaining, setTimeRemaining] = useState(5);
-  const [startGame, setStartGame] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(timerVar);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
 
   function handleChange(event) {
     const { value } = event.target;
     setText(value);
   }
-  function handleClick() {
-    console.log("working");
-    setStartGame((prevState) => (prevState = true));
-  }
-  useEffect(() => {
-    if (startGame) {
-      setTimeout(() => {
-        if (timeRemaining !== 0) {
-          setTimeRemaining((time) => time - 1);
-        } else {
-          return timeRemaining;
-        }
-      }, 1000);
-    }
-  }, [timeRemaining]);
 
+  useEffect(() => {
+    setTimeout(() => {
+      if (isTimeRunning === true && timeRemaining !== 0) {
+        setTimeRemaining((time) => time - 1);
+      } else if (timeRemaining === 0) {
+        setIsTimeRunning(false);
+      }
+    }, 1000);
+  }, [timeRemaining, isTimeRunning]);
+
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(timerVar);
+    }
+  }, [isTimeRunning]);
   function calcWordNumber(words) {
     const number = words.trim().split(" ");
     const filteredWords = number.filter((word) => word !== "");
@@ -33,15 +34,33 @@ function App() {
     return filteredWords.length;
   }
 
+  function onStartClick() {
+    setIsTimeRunning((prevState) => true);
+    setText("");
+  }
+
   return (
     <div className="App">
       <h1>HOW FAST CAN YOU TYPE?</h1>
 
-      <textarea name={"text"} value={text} onChange={handleChange} />
+      <textarea
+        disabled={!isTimeRunning}
+        readOnly={!isTimeRunning}
+        name={"text"}
+        value={text}
+        onChange={handleChange}
+      />
       <h4>Time left: {timeRemaining}</h4>
 
-      <button onClick={handleClick}>START</button>
-      <h1>WORD COUNT: </h1>
+      <button onClick={onStartClick} disabled={isTimeRunning}>
+        START
+      </button>
+      <h1>
+        WORD COUNT:{" "}
+        {timeRemaining === 0 || timeRemaining === timerVar
+          ? calcWordNumber(text)
+          : "--"}
+      </h1>
     </div>
   );
 }
